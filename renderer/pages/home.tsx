8eb6096418f67ae9b7fda009c6c1be2f8046973a
Hehoku <hehoku@outlook.com>
2023-05-01 23:51:26 +0800

    chore: sort tailwindcss class name

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -9,13 +9,13 @@ function Home() {
       <Head>
         <title>AoA</title>
       </Head>
-      <div className="flex items-center justify-center flex-col gap-20 text-2xl w-full text-center">
+      <div className="flex w-full flex-col items-center justify-center gap-20 text-center text-2xl">
         <p className="text-2xl font-bold">AoA</p>
-        <div className="bg-gray-300 w-48 h-48 rounded-full flex cursor-pointer items-center justify-center">
+        <div className="flex h-48 w-48 cursor-pointer items-center justify-center rounded-full bg-gray-300">
           <p className="cursor-pointer">开始工作</p>
         </div>
       </div>
-      <div className="w-full flex-wrap flex justify-center mt-10">
+      <div className="mt-10 flex w-full flex-wrap justify-center">
         <Link href="/next">
           <a className="btn-blue">Go to next page</a>
         </Link>
