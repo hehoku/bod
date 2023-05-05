44be9c187ccc473c34b89d43b96db58dc043c011
Hehoku <hehoku@outlook.com>
2023-05-05 22:57:51 +0800

    feat: add log page

--- renderer/pages/log.tsx
+++ renderer/pages/log.tsx
@@ -4,23 +4,19 @@ import Link from "next/link";
 import { Settings } from "react-feather";
 import { motion } from "framer-motion";
 
-function Home() {
+function Log() {
   return (
     <React.Fragment>
       <Head>
         <title>AoA</title>
       </Head>
       <div className="flex w-full flex-col items-center justify-center gap-20 text-center text-2xl">
-        <p className="text-2xl font-bold">AoA</p>
-        <Link href="/log">
-          <div>
-            <BreatheCircle />
-          </div>
+        <Link href="/home">
+          <p className="text-2xl font-bold cursor-pointer">AoA</p>
         </Link>
+        <BreatheCircle />
       </div>
-      <div className="mt-10 flex w-full flex-wrap justify-center">
-        <p className="font-bold">20分钟后休息一下</p>
-      </div>
+      <div className="mt-10 flex w-full flex-wrap justify-center">Log Page</div>
       <div className="absolute bottom-10 left-10 cursor-pointer">
         <Settings />
       </div>
@@ -50,4 +46,4 @@ const BreatheCircle = () => {
   );
 };
 
-export default Home;
+export default Log;
